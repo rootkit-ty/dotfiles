86216fcb56d4be69391daf5a872922a2be26eb01
Ring 3 Rootkit.ty <kitty@rootkitty.tech>
2018-10-17 16:52:23 +1100

    More emacs config changes

--- Spacemacs/init.el
+++ Spacemacs/init.el
@@ -350,7 +350,6 @@ you should place your code here."
   (unless (file-exists-p (concat spacemacs-cache-directory "undo"))
     (make-directory (concat spacemacs-cache-directory "undo")))
 
-  (setq custom-file "~/.spacemacs.d/custom.el")
 
   (defun mu4e-compose-org-mail ()
     (interactive)
@@ -629,5 +628,27 @@ Today I learnt %?")
      (python        . t)
      ))
 
+  (setq custom-file "~/.spacemacs.d/custom.el")
+  (load-file custom-file)
+  (load-file "~/.spacemacs.d/private.el")
+
   (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'sauron-event-added-functions 'sauron-alert-el-adapter)
+  (sauron-start-hidden)
+  (pinentry-start)
+
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit ghub treepy graphql zoom yapfify yaml-mode xterm-color ws-butler winum which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package toc-org tagedit spaceline smeargle smart-tab slim-mode shell-pop scss-mode sauron sass-mode restart-emacs real-auto-save ranger rainbow-delimiters q4 pyvenv pytest pyenv-mode py-isort pug-mode pip-requirements pinentry persp-mode pcre2el paradox orgit org-super-agenda org-projectile org-present org-pomodoro org-mime org-download org-bullets org-board open-junk-file ob-async nlinum-relative neotree multi-term mu4e-maildirs-extension mu4e-alert move-text mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum livid-mode live-py-mode link-hint less-css-mode ledger-mode json-mode js2-refactor js-doc insert-shebang indent-guide hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-themes helm-swoop helm-pydoc helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-bbdb helm-ag google-translate golden-ratio gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fzf fuzzy flyspell-correct-helm flycheck-pos-tip flycheck-ledger flx-ido fish-mode fill-column-indicator fasd fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-snipe evil-search-highlight-persist evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-commentary evil-args evil-anzu eval-sexp-fu eshell-z eshell-prompt-extras esh-help epresent emmet-mode elisp-slime-nav elfeed-web elfeed-protocol elfeed-org elfeed-goodies dumb-jump doom-themes diminish diff-hl define-word cython-mode csv-mode company-web company-tern company-statistics company-shell company-anaconda column-enforce-mode coffee-mode clean-aindent-mode auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
