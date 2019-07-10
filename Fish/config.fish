833d93186dfff27ce6d73a9e828368e57945d1da
Ring 3 Rootkitty <kitty@rootkitty.tech>
2019-07-10 17:12:27 +1000

    Replace double quotes with single quotes
    This is to avoid problems with evaluation and escaping

--- Fish/config.fish
+++ Fish/config.fish
@@ -5,5 +5,12 @@ set -x EDITOR nvim
 # FZF default command
 set FZF_DEFAULT_COMMAND 'rg --files --hidden --follow'
 
-alias fgc="fzf_git_checkout_branch"
+alias fgc='fzf_git_checkout_branch'
 
+alias gpu='git stash push'
+alias gpo='git stash pop'
+alias awsp='aws_profile_select'
+
+function __fasd_run -e fish_preexec -d 'fasd takes record of the directories changed into'
+	command fasd --proc (command fasd --sanitize "$argv") > '/dev/null' 2>&1 &
+end
